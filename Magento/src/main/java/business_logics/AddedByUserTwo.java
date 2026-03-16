1761e56ddc3d2cf0681f41e8be90fb82aa9ea23f
sachin <sachinbijawad01@gmail.com>
2026-03-16 15:10:47 +0530

    user added two lines

--- Magento/src/main/java/business_logics/AddedByUserTwo.java
+++ Magento/src/main/java/business_logics/AddedByUserTwo.java
@@ -5,6 +5,9 @@ public class AddedByUserTwo {
 	public static void main(String[] args) {
 		
 		System.out.println("user one chages ");
+		System.out.println("Use two added ");
+		System.out.println("Use two added ");
+
 		
 	}
 
