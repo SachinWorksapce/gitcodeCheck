a927538e48d09e33d3ea49496b2bf7379908048b
sachin <sachinbijawad01@gmail.com>
2026-03-16 17:03:19 +0530

    Added by the user two in dev branch

---
+++ Magento/src/main/java/business_logics/AddedInBranchByUSerTwo.java
@@ -0,0 +1,10 @@
+package business_logics;
+
+public class AddedInBranchByUSerTwo {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Added by the user two in dev branch");
+	}
+
+}
